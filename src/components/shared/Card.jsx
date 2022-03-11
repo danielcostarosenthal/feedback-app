fbf75ef8f28ae4fbecf1d2b4552000caeb139878
daniel.costa.rosenthal <daniel.costa.rosenthal@gmail.com>
2022-03-11 22:50:01 +0100

    Updated Card component

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,28 +1,7 @@
-import PropTypes from 'prop-types'
 import { StyledCard } from '../styles/Card.styled'
 
 const Card = ({ children }) => {
-	const handleClick = (item) => {
-		console.log(`Rating: ${item.rating}`)
-		console.log(`Text: ${item.text}`)
-		console.log(`ID: ${item.id}`)
-	}
-
 	return <StyledCard>{children}</StyledCard>
-}
-
-// Card Default Props
-Card.defaultProps = {
-	item: {
-		rating: 0,
-		text: 'The feedback text is missing. Please reload application or try later.',
-		id: 1,
-	},
-}
-
-// Card Typechecking PropTypes
-Card.propTypes = {
-	item: PropTypes.object.isRequired,
 }
 
 export default Card
